4f1e2c90f157cbf635afee9da57db3e4fbcc93d1
Caleb Thurston <caleb.thurston@tylertech.com>
2026-05-02 17:21:43 -0400

    feat(init): Initial Commit

---
+++ Contents/mods/OverkillTraits/42/media/registries.lua
@@ -0,0 +1,57 @@
+OTAP = OTAP or {}
+--OTAP.CharacterTrait = {}
+
+
+--Character Traits
+
+OTAP.CharacterTrait = {
+    Example         = CharacterTrait.register("otap:example"),
+    Kleptomania     = CharacterTrait.register("otap:kleptomania"),
+    Pyromania       = CharacterTrait.register("otap:pyromania"),
+    Sanguinarian    = CharacterTrait.register("otap:sanguinarian"),
+    ItchyFinger     = CharacterTrait.register("otap:itchy_trigger_finger"),
+    FootballAthlete = CharacterTrait.register("otap:football_athlete"),
+    Creative        = CharacterTrait.register("otap:creative"),
+    HomeCooking     = CharacterTrait.register("otap:home_cooking"),
+    FreeSpirit      = CharacterTrait.register("otap:free_spirit"),
+    Plutomania      = CharacterTrait.register("otap:plutomania"),
+    CleanFreak      = CharacterTrait.register("otap:clean_freak"),
+    ColdBlooded     = CharacterTrait.register("otap:cold_blooded"),
+    HotBlooded      = CharacterTrait.register("otap:hot_blooded"),
+    CareFree        = CharacterTrait.register("otap:care_free"),
+    BookWorm        = CharacterTrait.register("otap:book_worm"),
+    Philistine      = CharacterTrait.register("otap:philistine"),
+    Hoarder         = CharacterTrait.register("otap:hoarder"),
+    Minimalist      = CharacterTrait.register("otap:minimalist"),
+    LoudMouth       = CharacterTrait.register("otap:loud_mouth"),
+    SoftSpoken      = CharacterTrait.register("otap:soft_spoken"),
+    Psychopath      = CharacterTrait.register("otap:psychopath"),
+    AnimalLover     = CharacterTrait.register("otap:animal_lover"),
+    Nyctophobia     = CharacterTrait.register("otap:nyctophobia"),
+    LineBacker      = CharacterTrait.register("otap:line_backer"),
+    Resourceful     = CharacterTrait.register("otap:resourceful"),
+    Piggish         = CharacterTrait.register("otap:piggish"),
+    Gobbler         = CharacterTrait.register("otap:gobbler"),
+    Nibbler         = CharacterTrait.register("otap:nibbler"),
+    Hyposmia        = CharacterTrait.register("otap:hyposmia"),
+    Hyperosmia      = CharacterTrait.register("otap:hyperosmia"),
+    Astraphobia     = CharacterTrait.register("otap:astraphobia"),
+    Vehophobia      = CharacterTrait.register("otap:vehophobia"),
+    Gephyrophobia   = CharacterTrait.register("otap:gephyrophobia"),
+    Pyrophobia      = CharacterTrait.register("otap:pyrophobia"),
+    Ichthyophobia   = CharacterTrait.register("otap:ichthyophobia"),
+    Cipophobia      = CharacterTrait.register("otap:cipophobia"),
+    Ombrophobia     = CharacterTrait.register("otap:ombrophobia"),
+    Cannibal        = CharacterTrait.register("otap:cannibal"),
+    Alcoholic       = CharacterTrait.register("otap:alcoholic"),
+    InfinitePoints  = CharacterTrait.register("otap:infinite_points"),
+    God             = CharacterTrait.register("otap:god"),
+    Vegan           = CharacterTrait.register("otap:vegan"),
+    Carnivore       = CharacterTrait.register("otap:carnivore"),
+}
+
+-- Character Professions
+
+OTAP.CharacterProfession = {
+    
+}
