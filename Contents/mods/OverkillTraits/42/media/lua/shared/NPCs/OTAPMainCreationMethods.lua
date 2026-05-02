af5c99c57f22f6bb6b5cead96e6c957e7fffe81a
Caleb Thurston <caleb.thurston@tylertech.com>
2026-05-02 17:54:23 -0400

    chore(traits): remove development testing trait functionality from `main` branch

--- Contents/mods/OverkillTraits/42/media/lua/shared/NPCs/OTAPMainCreationMethods.lua
+++ Contents/mods/OverkillTraits/42/media/lua/shared/NPCs/OTAPMainCreationMethods.lua
@@ -1,14 +1,1 @@
 OTAPBaseGameCharacterDetails = {}
-
--- require "Items/SpawnItems";
-
-OTAPBaseGameCharacterDetails.NewCharacterInit = function(playerNum, character)
-    local player = getSpecificPlayer(playerNum); -- playerNum is like a player ID?
-
-    if player:hasTrait(OTAP.CharacterTrait.God) then
-        player:getInventory():AddItem("Base.Pistol")
-    end
-
-end
-
-Events.OnCreatePlayer.Add(OTAPBaseGameCharacterDetails.NewCharacterInit);
